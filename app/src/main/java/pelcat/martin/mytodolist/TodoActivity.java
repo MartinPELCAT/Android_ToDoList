f2d7f91d7663c3d6a65ea8899a9de002108ce8e2
Martin PELCAT <pelcatm@iut-acy.local>
2018-01-15 17:45:22 +0100

    Oué

--- app/src/main/java/pelcat/martin/mytodolist/TodoActivity.java
+++ app/src/main/java/pelcat/martin/mytodolist/TodoActivity.java
@@ -1,12 +1,17 @@
 package pelcat.martin.mytodolist;
 
 import android.app.Activity;
+import android.app.LauncherActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by pelcatm on 15/01/2018.
@@ -14,6 +19,7 @@ import android.widget.Toast;
 public class TodoActivity extends Activity implements View.OnClickListener {
     private Button valider;
     private Button vider;
+    private ArrayList<String> listItems= new ArrayList();
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -22,6 +28,8 @@ public class TodoActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.task_layout);
         valider = (Button) findViewById(R.id.valider);
         valider.setOnClickListener(this) ;
+        ListView view = (ListView) findViewById(R.id.listeview);
+        view.setAdapter(new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,listItems));
 
         vider = (Button) findViewById(R.id.clearList);
         vider.setOnClickListener(this) ;
@@ -32,12 +40,15 @@ public class TodoActivity extends Activity implements View.OnClickListener {
         if(v.getId() == R.id.valider)
         {
             EditText edit = (EditText) findViewById(R.id.editText);
-            TextView view = (TextView) findViewById(R.id.textView);
-            view.setText(edit.getText().toString());
+            ListView view = (ListView) findViewById(R.id.listeview);
+            listItems.add(edit.getText().toString());
+            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                    android.R.layout.simple_list_item_1, listItems);
+            view.setAdapter(adapter);
+
         }
         else{
-            TextView view = (TextView) findViewById(R.id.textView);
-            view.setText("");
+            ListView view = (ListView) findViewById(R.id.listeview);
         }
 
 
